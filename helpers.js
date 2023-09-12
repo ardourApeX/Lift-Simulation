4df5f8d086539e82d74d99ad17679982bc09bc24
Joy <joygupta501@gmail.com>
2023-09-12 22:34:14 +0530

    Feat : Completed code

--- helpers.js
+++ helpers.js
@@ -5,15 +5,79 @@ function viewElement(element) {
 	element.classList.remove('hidden');
 }
 
-function nearestLift(floornumber) {
-	let closestLift = 0;
-	let closestFloor = liftStatus[0].floorNumber;
-	liftStatus.forEach((lift) => {
+function delayedFunction(duration, callback) {
+	setTimeout(callback, duration);
+}
+function closestLift(floornumber) {
+	let leastFloorDifference = window.noOfFloors - 1; //Initial value to compare with
+	let nearestLift = -1; //Initial value to compare with
+	for (let [key, value] of Object.entries(liftStatus)) {
 		if (
-			Math.abs(lift.floorNumber - floornumber) <
-			Math.abs(closestFloor - floornumber)
+			!value.moving &&
+			!value.transitioning &&
+			Math.abs(value.floorNumber - floornumber) < leastFloorDifference
 		) {
-			closestFloor = lift.floorNumber;
+			leastFloorDifference = Math.abs(value.floorNumber - floornumber);
+			nearestLift = key;
 		}
+	}
+
+	return nearestLift;
+}
+
+function moveLift(liftNumber, floorNumber, direction) {
+	liftStatus[liftNumber].moving = true;
+	if (liftStatus[liftNumber].floorNumber === floorNumber) {
+		liftStatus[liftNumber].moving = false;
+		return openLiftGate(liftNumber);
+	}
+	const lift = document.getElementById('lift-' + liftNumber);
+	lift.style.translate = `0px -${
+		floorHeight * parseInt(floorNumber) + 2 * floorNumber
+	}px`;
+
+	lift.style.transitionDuration = '2s';
+	lift.style.transitionTimingFunction = 'ease-in-out';
+	setTimeout(() => {
+		liftStatus[liftNumber].floorNumber = parseInt(floorNumber);
+		liftStatus[liftNumber].moving = false;
+		openLiftGate(liftNumber);
+	}, 2000);
+}
+function callLift(floorNumber, direction) {
+	const nearestLift = closestLift(floorNumber);
+	nearestLift != -1 && moveLift(nearestLift, floorNumber, direction);
+}
+
+function closeLiftGate(liftNumber) {
+	const parentDiv = document.getElementById(`lift-${liftNumber}`);
+	// Get the child divs
+	const childDivs = parentDiv.querySelectorAll('div');
+
+	// Lift Door
+	childDivs[0].classList.remove('left-opening-gate');
+	childDivs[0].classList.add('left-closing-gate');
+
+	// Right Door
+	childDivs[1].classList.remove('right-opening-gate');
+	childDivs[1].classList.add('right-closing-gate');
+
+	delayedFunction(1000, () => {
+		childDivs[0].classList.remove('left-closing-gate');
+		childDivs[1].classList.remove('right-closing-gate');
+		liftStatus[liftNumber].transitioning = false;
+	});
+}
+function openLiftGate(liftNumber) {
+	liftStatus[liftNumber].transitioning = true;
+
+	const parentDiv = document.getElementById(`lift-${liftNumber}`);
+	// Get the child divs
+	const childDivs = parentDiv.querySelectorAll('div');
+	childDivs[1].classList.add('right-opening-gate');
+	childDivs[0].classList.add('left-opening-gate');
+
+	delayedFunction(1000, () => {
+		closeLiftGate(liftNumber);
 	});
 }
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Global Variables
 window.softPadding = 10;
-window.levelHalfHeight = 50;
+window.floorHeight = 100;
 window.noOfLifts = 0;
 window.noOfFloors = 0;
 window.liftStatus = {};
@@ -15,38 +15,60 @@ const lifts = document.getElementById('lifts');
 function onSubmitHandler() {
 	window.noOfLifts = parseInt(liftInput.value || '0');
 	window.noOfFloors = parseInt(floorInput.value || '0');
+	if (window.noOfFloors === 0 || window.noOfLifts === 0) {
+		return alert('Please enter valid no of floors or lifts');
+	}
 	renderFloorsAndLifts();
 }
 
+function liftButtonHandler(event) {
+	const target = event.target;
+
+	if (target.tagName === 'BUTTON') {
+		const buttonType = target.innerText; // 'Up' or 'Down'
+		const floorNumber = target.parentElement.parentElement.id.split('-')[1];
+		callLift(parseInt(floorNumber), buttonType);
+	}
+}
+
 function renderFloorsAndLifts() {
-	hideElement(form);
-	let innerHTML = '';
+	hideElement(form); //Hide form
+
+	let floorInnerHTML = '';
 	for (let i = 0; i < noOfFloors; i++) {
+		//Adding floors in backward direction
+		floorInnerHTML =
+			`
+				<div id=${'level-' + i} class="level">
+					<div class="button">
+						${i == noOfFloors - 1 ? '' : '<button>Up</button>'}
+						${i == 0 ? '' : '<button>Down</button>'}
+						<h3>floor ${i}</h3>
+					</div>
+				</div>
+			` + floorInnerHTML;
+	}
+
+	let liftInnerHTML = '';
+	for (let i = 0; i < noOfLifts; i++) {
 		liftStatus[i] = {
 			moving: false,
 			transitioning: false,
 			floorNumber: 0,
 		};
-		innerHTML += `
-            <div id=${'level-' + i} class="level">
-				<div class="button">
-                    ${i == 0 ? '' : '<button>Up</button>'}
-					${i == noOfFloors - 1 ? '' : '<button>Down</button>'}
-				</div>
-				<div class="floor">
-					<hr />
-					floor
-				</div>
+		liftInnerHTML += `<div id=${'lift-' + i} class="lift">
+				<div></div>
+				<div></div>
 			</div>`;
 	}
 
-	let liftInnerHTML = '';
-	for (let i = 0; i < noOfLifts; i++) {
-		liftInnerHTML += `<div class="lift"></div>`;
-	}
 	lifts.innerHTML = liftInnerHTML;
-	building.innerHTML += innerHTML;
-	viewElement(lifts);
-	viewElement(building);
+	building.innerHTML += floorInnerHTML;
+	building.style.width = `${noOfLifts * 60 + 200}px`; //Building width based on input
+
+	viewElement(building); //Make building visible
+
+	//Attached event handler
+	building.addEventListener('click', liftButtonHandler);
 }
 hideElement(building);
